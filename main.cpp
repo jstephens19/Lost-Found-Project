6fba156a4e1366704905b88439f531282bc730ab
Alejandro-260 <alejandrozc260@gmail.com>
2025-03-09 20:51:10 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,11 @@
 #include <string>
 #include <iostream>
 
-class item {
+class item {        // Item class
   public:
     string name;    // Name of item
     int itemNum;    // Unique number asigned to item
 
-  
 }
 
 int main ()
