daf59bd02af8e0297704ca4011b5610fb4246d45
Alejandro-260 <alejandrozc260@gmail.com>
2025-03-09 20:33:11 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
+// Main
 
+#include <string>
+#include <iostream>
+
+class item {
+  public:
+    string name;    // Name of item
+    int itemNum;    // Unique number asigned to item
+
+  
+}
+
+int main ()
+{
+  
+}
