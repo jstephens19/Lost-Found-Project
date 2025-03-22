89e2e133796a8db517ea0125e21c46569af843ab
jstephens19 <jstephens17@angelo.edu>
2025-03-22 14:39:33 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,20 @@
 
 #include <string>
 #include <iostream>
+#include<vector>
 
-class item {        // Item class
+class Item {        // Item class
   public:
     string name;    // Name of item
     int itemNum;    // Unique number asigned to item
+  //contructor to initialize item
+  Item(string itemName, int number) : name(itemName), itemNum(number){}
+};
 
-}
+//function prototypes
+void addItem(vector<Item>& items);    //add new items
+void displayItems(const vector<Item>& items);    //display items
+void searchItem(const vector<Item>& items);    //search items
 
 int main ()
 {
