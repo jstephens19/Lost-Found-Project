caf0b273069dc0c32ec81f656576095b13b7e87b
jstephens19 <jstephens17@angelo.edu>
2025-03-22 15:14:53 -0500

    Update itemDisplay.cpp

--- itemDisplay.cpp
+++ itemDisplay.cpp
@@ -1,1 +1,17 @@
+//Display funtion
+void displayItems(const vector<Item>& items)
+{
+    //Check if there are any lost items
+    if(items.empty())
+    {
+        cout<< "No items in the Lost & Found" << endl;
+        return;
+    }
 
+    //display all lost & found items
+    cout << "\n Lost & Found Items:" << endl;
+    for(size_t i = 0; i < items.size(); i++)
+    {
+      cout<< "Item Name: " << items[i].name << ", Item Number: " << items[i].itemNum << endl;
+    }
+}
