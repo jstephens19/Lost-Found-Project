c1dd9102800360bb2077f8d34141fe9b0f4d67f5
jstephens19 <jstephens17@angelo.edu>
2025-04-10 14:15:13 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,8 +16,37 @@ class Item {        // Item class
 void addItem(vector<Item>& items);    //add new items
 void displayItems(const vector<Item>& items);    //display items
 void searchItem(const vector<Item>& items);    //search items
+void addLostItems(int user_id, const string& item_name, const string& description, const string& lost_date, const string& location)
 
 int main ()
 {
-  
+        vector<Item> items;
+    int choice;
+
+    while (true) 
+    {
+        cout << "\nLost & Found Menu:\n";
+        cout << "1. Add Item\n2. Display Items\n3. Search Item\n4. Exit\n";
+        
+        cin >> choice;
+        cin.ignore();
+
+        switch (choice)
+          {
+            case 1:
+                addItem(items);
+                break;
+            case 2:
+                displayItems(items);
+                break;
+            case 3:
+                searchItem(items);
+                break;
+            case 4:
+                cout << "Exiting..." << endl;
+                return 0;
+            default:
+                cout << "Invalid choice. Try again." << endl;
+        }
+    }
 }
